eba1a578625d276bde9d5c253749d47becc19f9a
The Thought Emporium <jaaz95@gmail.com>
2023-02-23 14:41:40 -0500

    Delete maincode directory

--- maincode/ButtonClass.ino
+++
@@ -1,7 +0,0 @@
-void doubleclick() {  // what happens when button is double-clicked (Future Features)
-
-}
-void singleclick() {  // what happens when the button is clicked (Future Features)
-}
-void longclick() {  // what happens when buton is long-pressed (Future Features)
-}
